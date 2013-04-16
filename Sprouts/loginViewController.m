344c06c5c41fb59ab2e916faf1abd545fc52a621
Arthur Che <che@ischool.berkeley.edu>
2013-04-15 22:02:53 -0700

    Login button connects to FacebookSDK and creates a PFUser in Parse

--- Sprouts/loginViewController.m
+++ Sprouts/loginViewController.m
@@ -29,9 +29,27 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
 }
+
+//- (IBAction)loginButtonPushed:(UIButton *)sender {
+//    sproutsViewController *mainView = [[sproutsViewController alloc] init];
+//    [self.navigationController pushViewController:mainView animated:YES];
+//}
+
+
 - (IBAction)loginButtonPushed:(UIButton *)sender {
-    sproutsViewController *mainView = [[sproutsViewController alloc] init];
-    [self.navigationController pushViewController:mainView animated:YES];
+    // The permissions requested from the user
+    NSArray *permissionsArray = @[ @"email"];
+    
+    // Login PFUser using Facebook
+    [PFFacebookUtils logInWithPermissions:permissionsArray block:^(PFUser *user, NSError *error) {
+        if (!user) {
+            NSLog(@"Uh oh. The user cancelled the Facebook login.");
+        } else if (user.isNew) {
+            NSLog(@"User signed up and logged in through Facebook!");
+        } else {
+            NSLog(@"User logged in through Facebook!");
+        }
+    }];
 }
 
 - (void)didReceiveMemoryWarning
