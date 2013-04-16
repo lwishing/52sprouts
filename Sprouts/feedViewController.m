1448e4cb2654000550e0ac81b310bf6ceb21190b
Laura Wishingrad <lawishing@gmail.com>
2013-04-16 16:15:58 -0700

    removed unnecessary code in feedView

--- Sprouts/feedViewController.m
+++ Sprouts/feedViewController.m
@@ -26,30 +26,7 @@
     // Disable middle tab
     [[[[[self tabBarController]viewControllers]objectAtIndex:1]tabBarItem]setEnabled:false];
     
-//    // Programmatically add Schedule button to UI
-//    UIButton* scheduleButton = [UIButton buttonWithType:UIButtonTypeCustom];
-//    UIImage *buttonImage = [UIImage imageNamed:@"schedule_button.png"];
-//    //    UIImage *highlightImage = [UIImage imageNamed:@"sprout_pressed.png"];
-//    scheduleButton.frame = CGRectMake(0.0, 44.0, buttonImage.size.width, buttonImage.size.height);
-//    [scheduleButton setBackgroundImage:buttonImage forState:UIControlStateNormal];
-//    //    [sproutButton setBackgroundImage:highlightImage forState:UIControlStateHighlighted];
-//    //    [sproutButton setTitle:@"Sprout" forState:UIControlStateNormal];
-//    //    [sproutButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
-//    [scheduleButton addTarget:self action:@selector(scheduleButtonPressed:) forControlEvents:UIControlEventTouchUpInside];
-//    
-//    [self.view addSubview:scheduleButton];
-    
 }
-
-//- (IBAction)scheduleButtonPressed:(UIButton *)sender
-//{
-//    UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"MainStoryboard" bundle:nil];
-//    scheduleViewController *scheduleController = [storyboard instantiateViewControllerWithIdentifier:@"schedule"];
-////    [self setModalTransitionStyle:UIModalTransitionStyleFlipHorizontal];
-////    [self presentViewController:scheduleController animated:YES completion:nil];
-//    [self.navigationController pushViewController:scheduleController animated:YES];
-//}
-
 
 - (void)didReceiveMemoryWarning
 {
