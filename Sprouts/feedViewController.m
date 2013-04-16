e839d29a38db7c3724ef77e67dd328031d2284ee
Gilbert Hernandez <gahernandez@ischool.berkeley.edu>
2013-04-16 15:16:51 -0700

    Rename "first" and "second" view controllers to "feed" and "profile"

--- Sprouts/feedViewController.m
+++ Sprouts/feedViewController.m
@@ -6,14 +6,14 @@
 //  Copyright (c) 2013 Laura Wishingrad. All rights reserved.
 //
 
-#import "sproutsFirstViewController.h"
+#import "feedViewController.h"
 #import "scheduleViewController.h"
 
-@interface sproutsFirstViewController ()
+@interface feedViewController ()
 
 @end
 
-@implementation sproutsFirstViewController
+@implementation feedViewController
 
 - (void)viewDidLoad
 {
