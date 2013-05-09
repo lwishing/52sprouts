faf821e93e9b79a32d03ae998c4599e00e08f75c
Arthur Che <che@ischool.berkeley.edu>
2013-05-09 11:26:12 -0700

    -removed infinite scroll

--- Sprouts/TimelineViewController.m
+++ Sprouts/TimelineViewController.m
@@ -128,16 +128,16 @@
 }
 
 
-- (void)scrollViewDidScroll:(UIScrollView *)scrollView {
-
-    // Infinite scroll
-    if (scrollView.contentSize.height - scrollView.contentOffset.y < (self.view.bounds.size.height)) {
-        if (![self isLoading]) {
-            [self loadNextPage];
-        }
-    }
-
-}
+//- (void)scrollViewDidScroll:(UIScrollView *)scrollView {
+//
+//    // Infinite scroll
+//    if (scrollView.contentSize.height - scrollView.contentOffset.y < (self.view.bounds.size.height)) {
+//        if (![self isLoading]) {
+//            [self loadNextPage];
+//        }
+//    }
+//
+//}
 
  // Override to customize what kind of query to perform on the class. The default is to query for
  // all objects ordered by createdAt descending.
